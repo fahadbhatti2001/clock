9612398e9e0559f02a07e3382c8be57c075c529c
Fahad Bhatti <fahadqayyumbhatti2001@gmail.com>
2023-05-06 01:37:40 +0500

    Initial

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+export default function DigitalClock() {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const hours = currentTime.getHours().toString().padStart(2, '0');
+    const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+    const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+
+    return (
+        <h1 className="text-9xl font-bold text-center">
+            {hours}:{minutes}:{seconds}
+        </h1>
+    );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import DigitalClock from '@/DigitalClock'
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <DigitalClock />
+    </div>
+  )
+}
+
+export default Home
